0323967db460e018103ebf641e331ab749a69cad
Scanf-s <calzone0404@gmail.com>
2025-02-12 02:56:04 +0900

    버그 수정 시도

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+class CommonTestCase(APITestCase):
+
+    def setUp(self):
+        return super().setUp()
+
+    def test_health_check(self):
+        response = self.client.get(path=reverse("health"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
