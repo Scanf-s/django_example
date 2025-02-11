20cdeb80f7b6a247e39d335a6223c2981ca65b0a
SULLUNG <calzone0404@gmail.com>
2025-02-12 03:00:05 +0900

    코드 리뷰 요청 (#15)
    * readme 수정
    
    * Update Readme.md
    
    * deploy 스크립트 수정
    
    * 버그 수정 시도
    
    * code lint

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CommonTestCase(APITestCase):
+
+    def setUp(self):
+        return super().setUp()
+
+    def test_health_check(self):
+        response = self.client.get(path=reverse("health"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
