69b0480425c1f28eb96194235aa69bb46f6f96a7
Scanf-s <calzone0404@gmail.com>
2025-02-12 02:57:11 +0900

    code lint

--- common/tests.py
+++ common/tests.py
@@ -1,6 +1,7 @@
-from rest_framework.test import APITestCase
-from rest_framework import status
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 
 class CommonTestCase(APITestCase):
 
