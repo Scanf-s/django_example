fa69ad638eebf4dca18f4a508e58c67c0378a1dd
SULLUNG <calzone0404@gmail.com>
2025-02-11 22:22:02 +0900

    코드 리뷰 요청 (#5)
    * update readme and dockercompose
    
    * create github actions script
    
    * create github actions script (but can be modified after)
    
    * github actions script 수정
    
    * add terraform
    
    * Update Readme.md
    
    * github actions 자동 PR 생성 테스트 1
    
    * github actions 자동 PR 생성 테스트 2
    
    * github actions 자동 PR 생성 테스트 3
    
    * github actions 자동 PR 생성 테스트 4
    
    * github actions 자동 PR 생성 테스트 5
    
    * github actions 자동 PR 생성 테스트 6
    
    * github actions 자동 PR 생성
    
    * 코드린트 및 테스트 작업 추가
    
    * modify envs
    
    * 오타 수정
    
    * 코드린트, 테스트 실행 결과 메세지 생성 테스트
    
    * 코드린트, 테스트 실행 결과 메세지 생성 테스트 2
    
    * 코드린트, 테스트 실행 결과 메세지 생성 테스트 3
    
    * 코드린트, 테스트 실행 결과 메세지 생성 테스트 4
    
    * download-artifact 버전 수정
    
    * upload-artifact 버전 수정
    
    * artifact 파일 경로 수정
    
    * upload 및 download 파일명 수정 시도 1
    
    * upload 및 download 파일명 수정 시도 2
    
    * upload 및 download 파일명 수정 시도 3
    
    * upload 및 download 파일명 수정 시도 4
    
    * upload 및 download 파일명 수정 시도 5
    
    * upload 및 download 파일명 수정 시도 6
    
    * create vpc terraform script
    
    * Readme 수정, github actions deploy 스크립트 수정, terraform ecr 추가
    
    * deploy.yml 스크립트 수정
    
    * 자동 배포 테스트 1

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,4 +17,7 @@ resource "aws_eip" "library_backend_fixed_ipv4" {
   tags = {
     Name = "library_eip"
   }
+
+output "ec2_public_ipv4" {
+  value = aws_eip.library_backend_fixed_ipv4.public_ip
 }
